f8352d91e36b92f0677a55a7e33c4d2d9725235b
Ionut-Claudiu (John) Herciu <john.herciu1@nhs.net>
2020-06-17 14:17:55 +0100

    Separated local audit server interactions from the AWS implementation.

---
+++ src/main/java/uk/nhs/cdss/audit/config/RestClientConfig.java
@@ -0,0 +1,32 @@
+package uk.nhs.cdss.audit.config;
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@RequiredArgsConstructor
+public class RestClientConfig {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return restTemplateBuilder.build();
+    }
+
+    @Bean
+    public RestTemplate auditRestTemplate() {
+        // Currently only used for local-only services (i.e. audit server)
+        // a timeout of 50 should be acceptable locally
+        var timeout = Duration.ofMillis(50);
+        return new RestTemplateBuilder()
+                .setConnectTimeout(timeout)
+                .setReadTimeout(timeout)
+                .build();
+    }
+
+}
